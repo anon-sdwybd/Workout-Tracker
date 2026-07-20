ee4b6b95d0532f071daf06fa6b53b0a4c34a2c61
anon-sdwybd <phroghealer@gmail.com>
2026-07-20 21:54:37 +0800

    V1.2

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,4 +1,14 @@
-let workouts = [];
+const savedWorkouts = localStorage.getItem("workouts")
+let workouts = savedWorkouts?JSON.parse(savedWorkouts):[];
+let editingIndex = null;
+
+function SubmitWorkout(){
+    if (editingIndex !== null){
+        EditWorkout(editingIndex);
+    }else{
+        AddWorkout();
+    }
+}
 
 function AddWorkout(){
     let exercise = document.getElementById('Exercise').value;
@@ -6,27 +16,31 @@ function AddWorkout(){
     let reps = document.getElementById('Reps').value;
     let sets = document.getElementById('Sets').value;
 
-    let workout = {
-        exercise: exercise,
-        weight: weight,
-        reps: reps,
-        sets: sets
-    };
+    const workout = {
+        exercise,
+        weight,
+        reps,
+        sets
+    }
     
     workouts.push(workout);
+    localStorage.setItem("workouts", JSON.stringify(workouts));
     DisplayWorkouts();
+    document.getElementById("WorkoutForm").reset();
 }
 
 function DisplayWorkouts(){
     let output = "";
 
-    for (let workout of workouts){
+    for (let index = 0; index < workouts.length; index++){
+        const workout = workouts[index];
         output += `
         <div class="workoutcard">
             <h2>${workout.exercise}</h2>
             <hr>
             <p>${workout.weight}kg x ${workout.reps} reps</p>
             <p>${workout.sets} sets</p>
+            <button class="DeleteButton" onClick="DeleteWorkout(${index})">Delete</button> <button class="EditButton" onClick="StartEditing(${index})">Edit</button>
         </div>
        `;
     }
@@ -34,3 +48,35 @@ function DisplayWorkouts(){
 document.getElementById('WorkoutList').innerHTML = output;
 
 }
+
+function DeleteWorkout(index){
+    workouts.splice(index, 1);
+    localStorage.setItem("workouts", JSON.stringify(workouts));
+    DisplayWorkouts();
+}
+
+function EditWorkout(index){
+    let exercise = document.getElementById('Exercise').value;
+    let weight = document.getElementById('Weight').value;
+    let reps = document.getElementById('Reps').value;
+    let sets = document.getElementById('Sets').value;
+
+    const workout = {
+        exercise,
+        weight,
+        reps,
+        sets
+    }
+
+    workouts[index] = workout;
+    
+    localStorage.setItem("workouts", JSON.stringify(workouts));
+    DisplayWorkouts();
+    editingIndex = null;
+}
+
+function StartEditing(index){
+    editingIndex = index;
+}
+
+DisplayWorkouts();
